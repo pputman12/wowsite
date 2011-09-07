d4268382900d886ee075ded85d47e1a69c6284f3
Patrick Putman <putman.patrick@gmail.com>
2011-09-07 17:13:50 -0500

    Fixed a bug where new guilds could not be created by the same user

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -7,8 +7,8 @@ class Guild < ActiveRecord::Base
   belongs_to :guild
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  #validates_presence_of :email
+  #validates_uniqueness_of :email
   has_many :characters
   has_many :topics
   
