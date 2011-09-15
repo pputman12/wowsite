2a40d2b1bcceec193de9aa0791b5414690c68ae0
Patrick Putman <putman.patrick@gmail.com>
2011-09-15 18:43:31 -0500

    Remove some uneeded torquebox stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Guildsite::Application.routes.draw do
   resources :guilds do
     resources :characters 
   end
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
